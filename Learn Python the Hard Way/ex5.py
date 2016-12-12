f7f8ed9c8e8c06854a3162d5ed48f1fff2d6dec3
Jorge Perez <jorslu@gmail.com>
2016-12-12 13:23:28 -0500

    Study Drill 5.3

--- Learn Python the Hard Way/ex5.py
+++ Learn Python the Hard Way/ex5.py
@@ -6,9 +6,14 @@ eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
+# Study Drill 2
+height_cm = height * 2.54
+weight_kg = weight * 0.453592
+
 print "Let's talk about %s." % name
 print "He's %d inches tall." % height
 print "He's %d pounds heavy." % weight
+print "In Europe, He's %d centimeters and %d kilograms." % (height_cm, weight_kg)
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are unusually %s depending on the coffee." % teeth
