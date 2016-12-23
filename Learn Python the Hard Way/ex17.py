db3c40a42255e308c5da3e993edbd544ac722371
Jorge Perez <jorslu@gmail.com>
2016-12-23 11:47:21 -0500

    Study Drill 17.1 + 17.2

--- Learn Python the Hard Way/ex17.py
+++ Learn Python the Hard Way/ex17.py
@@ -6,12 +6,8 @@ script, from_file, to_file = argv
 print "Copying from {} to {}".format(from_file, to_file)
 
 # We could do these two on one line, how?
-in_file = open(from_file)
-indata = in_file.read()
+indata = open(from_file).read()
 
-print "The input file is {} bytes long".format(len(indata))
-
-print "Does the output file exist? {0!r}".format(exists(to_file))
 print "Ready, hit RETURN (or ENTER) to continue, CTRL-C to abort."
 raw_input()
 
@@ -21,4 +17,3 @@ out_file.write(indata)
 print "Alright, all done."
 
 out_file.close()
-in_file.close()
