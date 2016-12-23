7d5ff2cd3884cc3554bdb16f8f71e22e34f2a918
Jorge Perez <jorslu@gmail.com>
2016-12-23 11:53:35 -0500

    Literal Oneliner achieved. Oneliner Badge Unlocked

--- Learn Python the Hard Way/ex17.py
+++ Learn Python the Hard Way/ex17.py
@@ -1,12 +1,1 @@
-from sys import argv
-from os.path import exists
-
-script, from_file, to_file = argv
-
-print "Copying from {} to {}".format(from_file, to_file)
-print "Ready, hit RETURN (or ENTER) to continue, CTRL-C to abort."
-raw_input()
-
-open(to_file, 'w').write(open(from_file).read())
-
-print "Alright, all done."
+from sys import argv;from os.path import exists;script, from_file, to_file = argv;open(to_file, 'w').write(open(from_file).read())
