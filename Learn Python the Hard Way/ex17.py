ea201ad9838b6aa581dc8b31540c0e9638188395
Jorge Perez <jorslu@gmail.com>
2016-12-23 11:49:16 -0500

    Made it a one liner

--- Learn Python the Hard Way/ex17.py
+++ Learn Python the Hard Way/ex17.py
@@ -4,16 +4,9 @@ from os.path import exists
 script, from_file, to_file = argv
 
 print "Copying from {} to {}".format(from_file, to_file)
-
-# We could do these two on one line, how?
-indata = open(from_file).read()
-
 print "Ready, hit RETURN (or ENTER) to continue, CTRL-C to abort."
 raw_input()
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
+open(to_file, 'w').write(open(from_file).read())
 
 print "Alright, all done."
-
-out_file.close()
